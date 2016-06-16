3f8fde43f13853c194e4b3c3fcf1484677a897dc
Richard Davis <richarddavis@lauras-mbp.attlocal.net>
2016-06-16 13:11:53 -0500

    game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,13 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+if (cardOne === cardTwo) {
+	alert('You found a match!');
+} else if (cardOne !== cardThree || cardOne !== cardFour) {
+	alert('Sorry, try again');
+} else if (cardTwo !== cardThree || cardTwo !== cardFour) {
+	alert('Sorry, try again');
+} else (cardThree === cardFour) {
+	alert(You found a match!);
+}
