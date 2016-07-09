3eb0970c0613343687f79e64ae2759bd155da6da
Richard Davis <RichardDavis@Richards-MBP.attlocal.net>
2016-07-09 17:49:08 -0500

    memorygame-js

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,7 +3,7 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
+/*if (cardOne === cardTwo) {
 	alert('You found a match!');
 } else if (cardOne !== cardThree || cardOne !== cardFour) {
 	alert('Sorry, try again');
@@ -11,4 +11,15 @@ if (cardOne === cardTwo) {
 	alert('Sorry, try again');
 } else (cardThree === cardFour) {
 	alert(You found a match!);
+}*/
+
+function createBoard() {
+
+	for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    board.appendChild(cardElement);
+  }
 }
+
+createBoard();
