a185c3ddafb2d581357e87dc4c0b7d080fc2b236
Richard Davis <RichardDavis@Richards-MBP.attlocal.net>
2016-07-20 14:22:31 -0500

    final

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,10 @@
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
 
+var board = document.getElementById('game-board');
 
 function createBoard() {
 
-var board = document.getElementById('game-board');
 
 for (var i=0; i<cards.length; i++) {
 	var cardElement = document.createElement('div');
@@ -13,14 +13,17 @@ for (var i=0; i<cards.length; i++) {
 
 	cardElement.setAttribute('data-card', cards[i]);
 	cardElement.addEventListener('click', isTwoCards);
-}
+	board.appendChild(cardElement);
+	board.appendChild(cardElement);
+  }
 }
 function isTwoCards() {
 	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(this.getAttribute('data-card'));
 	if (this.getAttribute('data-card') === 'king') {
-     cardElement.innerHTML = "<img src='https://drive.google.com/file/d/0B2_J-1zoxJQiZjh3bi1seFBBczQ/view?usp=sharing'>";
+     this.innerHTML = "<img src='https://pixabay.com/static/uploads/photo/2015/08/11/11/57/clubs-884194_960_720.png'>";
   } else {
-  	cardElement.innerHTML = "<img src='https://drive.google.com/file/d/0B2_J-1zoxJQiNE0xOWxWaWp6Rjg/view?usp=sharing'>";
+  	this.innerHTML = "<img src='https://pixabay.com/static/uploads/photo/2015/08/11/11/57/clubs-884198_960_720.png'>";
 
   }
 
@@ -30,16 +33,12 @@ function isTwoCards() {
 		cardsInPlay = [];
 	}
 }
-function isMatch() {
-	if (card[0] === card[1]) {
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
 	alert("You found a match!");
-} else if (card[0] !== card[2] || card[0] !== Card[3]) {
+} else {
 	alert("Sorry, try again.");
-} else if (card[1] !== card[2] || card[1] !== card[3]) {
-	alert("Sorry, try again.");
-} else if (card[2] === card[3]) {
-	alert("You found a match!");
-}
+  }
 }
 createBoard();
 
